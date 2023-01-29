a0303c8f78200a1f7a8427162c1624a7a1f44d10
“ShubhamvShrikhande” <shubhamvshri99@gmail.com>
2023-01-29 14:45:10 +0530

    Welcome to the Line Comparison

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC1_LineComparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the Line Comparison Computation Program");
+
+        }
+    }
+}
