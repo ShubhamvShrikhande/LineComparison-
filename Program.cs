02301bb4f806dbd5b46102ba90458b33d3ffe5e1
“ShubhamvShrikhande” <shubhamvshri99@gmail.com>
2023-01-29 15:01:07 +0530

    calculate Length

--- Program.cs
+++ Program.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace UC1_LineComparison
+namespace LineComparison
 {
     internal class Program
     {
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Welcome to the Line Comparison Computation Program");
+            Console.WriteLine("Enter the all Points");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            double l1 = Math.Pow((x2 - x1), 2);
+            double l2 = Math.Pow((y2 - y1), 2);
+            double length = l1 + l2;
+            double lengthOfLine = Math.Sqrt(length);
+
+            Console.WriteLine("Lenth Of The Line is:" + lengthOfLine);
 
         }
     }
